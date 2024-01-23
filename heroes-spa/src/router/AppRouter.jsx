e03e23ab2892a5792d4174788116d46e4e768253
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-01-23 11:23:57 -0400

    place the class active

--- heroes-spa/src/router/AppRouter.jsx
+++ heroes-spa/src/router/AppRouter.jsx
@@ -3,10 +3,12 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { MarvelPage } from "../heroes/pages/MarvelPage";
 import { DcPage } from "../heroes/pages/DcPage";
 import { LoginPages } from "../auth/pages/LoginPages";
+import { Navbar } from "../ui/components";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="marvel" element={<MarvelPage />} />
         <Route path="dc" element={<DcPage />} />
