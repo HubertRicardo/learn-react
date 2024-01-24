136aa86fdf1807f9af378b201440cdc899f439b3
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-01-24 01:33:03 -0400

    create componente search

--- heroes-spa/src/heroes/components/HeroCard.jsx
+++ heroes-spa/src/heroes/components/HeroCard.jsx
@@ -12,7 +12,7 @@ export const HeroCard = ({
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
   return (
-    <div className="col">
+    <div className="col  animate__animated animate__fadeIn">
       <div className="card">
         <div className="row no-gutter">
           <div className="col-4">
@@ -22,6 +22,8 @@ export const HeroCard = ({
             <div className="card-body">
               <h5 className="card-title">{superhero}</h5>
               <p className="card-text">{alter_ego}</p>
+              <p className="card-text">{publisher}</p>
+
               {alter_ego !== characters && <p>{characters}</p>}
               <p className="card-text">
                 <small>{first_appearance}</small>
