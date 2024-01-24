999cf2bc3b15e2da9325409ab277579e64ad3678
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-01-23 20:12:33 -0400

    create a second router

--- heroes-spa/src/router/AppRouter.jsx
+++ heroes-spa/src/router/AppRouter.jsx
@@ -1,20 +1,14 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DcPage } from "../heroes/pages/DcPage";
 import { LoginPages } from "../auth/pages/LoginPages";
-import { Navbar } from "../ui/components";
+import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
 
 export const AppRouter = () => {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
-
         <Route path="login" element={<LoginPages />} />
-        <Route path="/" element={<Navigate to="/marvel" />} />
+        <Route path="/*" element={<HeroesRoutes />} />
       </Routes>
     </>
   );
