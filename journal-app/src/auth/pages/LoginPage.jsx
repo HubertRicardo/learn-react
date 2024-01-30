0fc286934535c761e034570ea618120b5c5a0688
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-01-30 17:20:37 -0400

    management of forms in Login

--- journal-app/src/auth/pages/LoginPage.jsx
+++ journal-app/src/auth/pages/LoginPage.jsx
@@ -1,13 +1,34 @@
 import { Link as RouterLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
+import { checkingAuthentication, startGoogleSignIn } from "../../store/auth";
 
 export const LoginPage = () => {
+  const dispatch = useDispatch();
+
+  const { onInputChange, email, password } = useForm({
+    email: " fernando@google.com",
+    password: "12313",
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log({ email, password });
+    dispatch(checkingAuthentication());
+  };
+
+  const onGoogleSignIn = () => {
+    console.log("onGoogle");
+    dispatch(startGoogleSignIn());
+  };
+
   return (
     <AuthLayout title="Log In">
-      <form action="">
+      <form action="" onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -15,6 +36,9 @@ export const LoginPage = () => {
               type="email"
               placeholder="correo@gmail.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -24,17 +48,20 @@ export const LoginPage = () => {
               type="password"
               placeholder="password"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Log In
               </Button>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" fullWidth onClick={onGoogleSignIn}>
                 <Google />
                 <Typography sx={{ ml: 1 }}>Google</Typography>
               </Button>
--- journal-app/src/store/auth/authSlice.js
+++ journal-app/src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
-    status: "checking", // "no-authenticated", "authenticated"
+    status: "no-authtenticated", //"checking", "no-authenticated", "authenticated"
     uid: null,
     email: null,
     displayName: null,
@@ -13,7 +13,9 @@ export const authSlice = createSlice({
   reducers: {
     login: (state, action) => {},
     logout: (state, payload) => {},
-    checkingCredentials: (state) => {},
+    checkingCredentials: (state) => {
+      state.status = "checking";
+    },
   },
 });
 
