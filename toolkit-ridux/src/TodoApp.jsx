8dd2ce7eb44b8e5c3d5944241dc98274953baa83
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-01-30 01:28:09 -0400

    consume the api with a custom hook

--- toolkit-ridux/src/TodoApp.jsx
+++ toolkit-ridux/src/TodoApp.jsx
@@ -1,11 +1,20 @@
+import { useGetTodosQuery } from "./store/apis/todosApi";
+
 export const TodoApp = () => {
+  const { data: todos = [], isLoading } = useGetTodosQuery();
   return (
     <>
       <h1>Todo - RTK Query</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading: {isLoading ? "True" : "False"}</h4>
 
-      <pre></pre>
+      <pre>....</pre>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
 
       <button>Next Todo</button>
     </>
