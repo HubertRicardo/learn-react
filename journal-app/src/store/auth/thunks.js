917d8e12c61d1612715f16d76db43e6a5c2a4e99
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-01-30 23:55:11 -0400

    install firebase

--- journal-app/src/store/auth/thunks.js
+++ journal-app/src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { singInWithGoogle } from "../../firebase/provider";
 import { checkingCredentials } from "./authSlice";
 
 export const checkingAuthentication = (email, password) => {
@@ -9,5 +10,8 @@ export const checkingAuthentication = (email, password) => {
 export const startGoogleSignIn = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
+
+    const result = singInWithGoogle();
+    console.log(result);
   };
 };
