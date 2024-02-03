0cfe47feac90c65386f9af48dcca363e8b7e16bd
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-02-02 21:25:25 -0400

    customizing the event box

--- calendar/src/calendar/pages/CalendarPage.jsx
+++ calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,52 @@
-import { Navbar } from "../components/Navbar";
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { addHours } from "date-fns";
+import { Navbar, CalendarEvent } from "../";
+import { localizer } from "../../helpers";
+
+const events = [
+  {
+    title: "cumpleanos",
+    note: "Hay qye comprar el pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Hubert",
+    },
+  },
+];
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    // console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: "#347CF7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+    return {
+      style,
+    };
+  };
+
   return (
     <>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+        eventPropGetter={eventStyleGetter}
+        components={{ event: CalendarEvent }}
+      />
     </>
   );
 };
