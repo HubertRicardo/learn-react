06ca986567159ca804428b3e3eef326b2977df89
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-02-02 22:38:24 -0400

    modal content

--- calendar/src/calendar/pages/CalendarPage.jsx
+++ calendar/src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import { addHours } from "date-fns";
-import { Navbar, CalendarEvent } from "../";
+import { Navbar, CalendarEvent, CalendarModal } from "../";
 import { localizer } from "../../helpers";
 import { useState } from "react";
 
@@ -69,6 +69,8 @@ export const CalendarPage = () => {
         onSelectEvent={onSelect}
         onView={onViewChanged}
       />
+
+      <CalendarModal />
     </>
   );
 };
