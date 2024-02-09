5ae9f4b726e46787045e29f4ae19b1c28b343af5
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-02-09 02:14:37 -0400

    config basic CORS

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { dbConnection } = require("./database/config");
+const cors = require("cors");
 require("dotenv").config();
 
 // create express server
@@ -7,6 +8,9 @@ const app = express();
 
 //Base de datos
 dbConnection();
+
+//CORS
+app.use(cors());
 
 // public directory
 app.use(express.static("public"));
