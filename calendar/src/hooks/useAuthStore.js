726947f07f58ab1d37b720406b648ec61333cf8d
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-02-18 23:41:33 -0400

    dispatch accion in startLogin

--- calendar/src/hooks/useAuthStore.js
+++ calendar/src/hooks/useAuthStore.js
@@ -1,15 +1,18 @@
 import { useDispatch, useSelector } from "react-redux";
 import { calendarApi } from "../api";
+import { onChecking, onLogin } from "../store";
 
 export const useAuthStore = () => {
   const { status, user, errorMessage } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const startLogin = async ({ email, password }) => {
-    console.log({ email, password });
+    dispatch(onChecking());
     try {
-      const resp = await calendarApi.post("/auth", { email, password });
-      console.log({ resp });
+      const { data } = await calendarApi.post("/auth", { email, password });
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      dispatch(onLogin({ name: data.name, uid: data.uid }));
     } catch (error) {
       console.log(error);
     }
