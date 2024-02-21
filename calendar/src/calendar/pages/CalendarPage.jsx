4a53d49310cde3e7f1847d14512e233172811825
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-02-20 23:43:42 -0400

    change color that other user

--- calendar/src/calendar/pages/CalendarPage.jsx
+++ calendar/src/calendar/pages/CalendarPage.jsx
@@ -10,9 +10,10 @@ import {
 } from "../";
 import { localizer } from "../../helpers";
 import { useEffect, useState } from "react";
-import { useCalendarStore, useUiStore } from "../../hooks";
+import { useAuthStore, useCalendarStore, useUiStore } from "../../hooks";
 
 export const CalendarPage = () => {
+  const { user } = useAuthStore();
   const { openDateModal } = useUiStore();
   const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
 
@@ -22,9 +23,11 @@ export const CalendarPage = () => {
 
   const eventStyleGetter = (event, start, end, isSelected) => {
     // console.log({ event, start, end, isSelected });
+    const isMyEvent =
+      user.uid === event.user._id || user.uid === event.user.uid;
 
     const style = {
-      backgroundColor: "#347CF7",
+      backgroundColor: isMyEvent ? "#347CF7" : "#465660",
       borderRadius: "0px",
       opacity: 0.8,
       color: "white",
