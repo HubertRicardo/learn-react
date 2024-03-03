9a384948653d24f9278555a6f22d293a8e388f55
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-03-02 23:52:56 -0400

    fixed pokemon ditto and solicitude

--- node-course/api-con-express/2.route.js
+++ node-course/api-con-express/2.route.js
@@ -1,32 +1,57 @@
 const http = require("node:http");
 
-//comonJS -> modulos clasicos
+// commonJS -> modulos clásicos de node
 const dittoJSON = require("./pokemon/ditto.json");
 
-// este es un ejemplo con http bruto sin usar un framework
 const processRequest = (req, res) => {
   const { method, url } = req;
+
   switch (method) {
     case "GET":
       switch (url) {
         case "/pokemon/ditto":
-          res.setHeader("Content-Type", "aplication/json; charset=utf-8");
+          res.setHeader("Content-Type", "application/json; charset=utf-8");
           return res.end(JSON.stringify(dittoJSON));
         default:
           res.statusCode = 404;
-          res.setHeader("Content-type ", "text/html; charset=utf-8");
+          res.setHeader("Content-Type", "text/html; charset=utf-8");
           return res.end("<h1>404</h1>");
       }
-      break;
 
-    default:
-      break;
+    case "POST":
+      switch (url) {
+        case "/pokemon": {
+          let body = "";
+
+          // escuchar el evento data
+          req.on("data", (chunk) => {
+            body += chunk.toString();
+          });
+
+          req.on("end", () => {
+            const data = JSON.parse(body);
+            // llamar a una base de datos para guardar la info
+            res.writeHead(201, {
+              "Content-Type": "application/json; charset=utf-8",
+            });
+
+            data.timestamp = Date.now();
+            res.end(JSON.stringify(data));
+          });
+
+          break;
+        }
+
+        default:
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/plain; charset=utf-8");
+          return res.end("404 Not Found");
+      }
   }
 };
 
 const server = http.createServer(processRequest);
 
-//aqui se llama para que aparesca
 server.listen(1234, () => {
-  console.log("Server listening on port http://localhost:1234");
+  console.log("server listening on port http://localhost:1234");
 });
