feb08b10fc42278a51a5cc7176987a517542bc45
Hubert Ricardo Leanos <leanoshubert@gmail.com>
2024-03-02 23:44:37 -0400

    create pokemon ditto file

--- node-course/api-con-express/2.route.js
+++ node-course/api-con-express/2.route.js
@@ -1,14 +1,21 @@
 const http = require("node:http");
 
+//comonJS -> modulos clasicos
+const dittoJSON = require("./pokemon/ditto.json");
+
 // este es un ejemplo con http bruto sin usar un framework
 const processRequest = (req, res) => {
   const { method, url } = req;
   switch (method) {
     case "GET":
       switch (url) {
-        case "/":
-          res.setHeader("Content-Type", "text/html; charset=utf-8");
-          return res.end("<h1>Mi pagina</h1>");
+        case "/pokemon/ditto":
+          res.setHeader("Content-Type", "aplication/json; charset=utf-8");
+          return res.end(JSON.stringify(dittoJSON));
+        default:
+          res.statusCode = 404;
+          res.setHeader("Content-type ", "text/html; charset=utf-8");
+          return res.end("<h1>404</h1>");
       }
       break;
 
